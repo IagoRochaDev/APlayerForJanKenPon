3ff5feac3e1297d940dad866e9ecfa70d9c3af42
IagoRochaDev <iagor.oliveira00@gmail.com>
2025-08-08 11:42:38 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/ifnmg/aluno/iro/kakashi/Kakashi.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package br.edu.ifnmg.aluno.iro.kakashi;
+
+import io.github.guisso.jankenpon.AbstractPlayer;
+import io.github.guisso.jankenpon.Move;
+
+/**
+ *
+ * @author iagor
+ */
+public class Kakashi
+        extends AbstractPlayer {
+
+    int contadorDerrotas;
+    Move myPreviousMove;
+
+    public Kakashi() {
+        this.contadorDerrotas = 0;
+        myPreviousMove = Move.NONE;
+    }
+
+    @Override
+    public String getDeveloperName() {
+        return "Iago Rocha Oliveira";
+    }
+
+    @Override
+    public Move makeMyMove(Move opponentPreviousMove) {
+        if (contadorDerrotas < 0 || contadorDerrotas > 9) {
+            contadorDerrotas = 0;
+        }
+        if (opponentPreviousMove == Move.NONE) {
+            return Move.SCISSORS;
+        }
+        if (myPreviousMove == Move.PAPER) {
+            if (opponentPreviousMove == Move.SCISSORS) {
+                contadorDerrotas++;
+            }
+        }
+        if (myPreviousMove == Move.ROCK) {
+            if (opponentPreviousMove == Move.PAPER) {
+                contadorDerrotas++;
+            }
+        }
+        if (myPreviousMove == Move.SCISSORS) {
+            if (opponentPreviousMove == Move.ROCK) {
+                contadorDerrotas++;
+            }
+        }
+        if (contadorDerrotas < 3) {
+            return jogadaQueGanha(opponentPreviousMove);
+        }
+        if (contadorDerrotas < 6) {
+            return jogadaQuePerde(opponentPreviousMove);
+        }
+        if (contadorDerrotas < 9) {
+            return opponentPreviousMove;
+        }
+        return Move.PAPER;
+    }
+
+    public Move jogadaQueGanha(Move ultimaJogada) {
+        if (ultimaJogada == Move.PAPER) {
+            return Move.SCISSORS;
+        }
+        if (ultimaJogada == Move.ROCK) {
+            return Move.PAPER;
+        }
+        if (ultimaJogada == Move.SCISSORS) {
+            return Move.ROCK;
+        }
+        return Move.SCISSORS;
+    }
+
+    public Move jogadaQuePerde(Move ultimaJogada) {
+        if (ultimaJogada == Move.PAPER) {
+            return Move.ROCK;
+        }
+        if (ultimaJogada == Move.ROCK) {
+            return Move.SCISSORS;
+        }
+        if (ultimaJogada == Move.SCISSORS) {
+            return Move.PAPER;
+        }
+        return Move.ROCK;
+    }
+
+}
